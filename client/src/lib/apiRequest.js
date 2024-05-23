32107a927fcea28a40522c9b76f1ac1e0daee207
hitiksha-patel <hitikshapatel23@gmail.com>
2024-05-23 14:47:13 -0400

    client base url changes

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8801/api",
+  baseURL: process.env.BASE_URL,
   withCredentials: true,
 });
 export default apiRequest;
